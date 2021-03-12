28e9c763cd3b0e63747d7c294e5ab296fac38d9e
SimonKenneth <smkk715@163.com>
2021-03-12 11:20:53 +0800

    实现了线速度的pid控制，但低速会存在轻微振荡

--- ifly_navigation/src/pid.py
+++ ifly_navigation/src/pid.py
@@ -4,10 +4,11 @@ import math
 from matplotlib import pyplot as plt
 import time
 import random
+import rospy
 
 
 class PID_t:
-    def __init__(self, kp, ki, kd):
+    def __init__(self, kp, ki, kd, int_duty=0.01, int_max=100, output_max=10,sub_ctrl = False):
         self.kp = kp
         self.ki = ki
         self.kd = kd
@@ -16,7 +17,14 @@ class PID_t:
         self.last_err = 0
         self.last_last_err = 0
         self.delta_value = 0
+        self.int_count = 0  # 积分量
         self.expect_value = 0
+        self.int_duty = int_duty  # 积分周期/s
+        self.curr_time = rospy.get_time()
+        self.last_time = 0
+        self.int_max = int_max  # 积分限额
+        self.output_max = output_max
+        self.sub_ctrl = sub_ctrl # 是否开启分段pid
 
     def get_output_delta(self, now_value, expect_value):
         """增量式PID计算方法"""
@@ -27,14 +35,30 @@ class PID_t:
         self.delta_value = self.kp * (self.now_err - self.last_err) + self.ki * \
                            self.now_err + self.kd * (self.now_err - 2 * self.last_err
                                                      + self.last_last_err)
-        # print (self.delta_value)
-        return self.now_value+self.delta_value
+        return self.now_value + self.delta_value
 
-    def get_output(self,now_value, expect_value):
+    def get_output(self, now_value, expect_value):
         self.now_err = expect_value - now_value
-        output = self.kp * self.now_err + 0 + self.kd * (self.now_err - self.last_err)
+        self.int_count += self.now_err
+        if self.int_count >= self.int_max:
+            self.int_count = self.int_max
+        if self.int_count <= - self.int_max:
+            self.int_count = -self.int_max
+
+        output = 0
+        if self.sub_ctrl == False:
+            output = self.kp * self.now_err + self.ki * self.int_count + self.kd * (self.now_err - self.last_err)
+        else:
+            if abs(self.now_err) > 0.2:
+                output = self.kp * self.now_err + self.ki * self.int_count + self.kd * (self.now_err - self.last_err)
+            else:
+                output = 0.01 * self.now_err + 0.15 * self.int_count +2 * (self.now_err - self.last_err)
+
+        if output > self.output_max:
+            output = self.output_max
+        if output < -self.output_max:
+            output = -self.output_max
         self.last_err = self.now_err
-        self.now_value = output
         return output
 
 
--- ifly_navigation/src/pure_pursuit.py
+++ ifly_navigation/src/pure_pursuit.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist
 import sys
 import tty
 import threading
-import select
+import select,signal
 import termios
 import numpy as np
 import math
@@ -24,7 +24,7 @@ dt = 0.1  # 时间间隔，单位：s
 L = 2.9  # 车辆轴距，单位：m
 
 
-def get_key(key_timeout):
+def get_key(key_timeout,settings):
     tty.setraw(sys.stdin.fileno())
     rlist, _, _ = select.select([sys.stdin], [], [], key_timeout)
     if rlist:
@@ -68,7 +68,7 @@ class PathFollower:
 
         # Read the linear acceleration of the robot IMU
         self.linear_acc_x = imu_data.linear_acceleration.x
-        
+
         # 通过对加速度积分得到速度 >>>实验测得不靠谱，改用坐标变化量<<<
         # if self.int_start_flag == 1:
         #     self.int_start_flag = 0
@@ -101,13 +101,22 @@ class PathFollower:
             self.last_x = self.x
             # print("linear_vel_x:%5.2f linear_acc_x:%5.2f angular_vel_z:%5.2f yaw:%5.2f" % (
             #     self.linear_vel_x, self.linear_acc_x, self.angular_vel_z, self.yaw))
-    
+
+
+def quit(signum, frame):
+    print ''
+    print 'stop fusion'
+    twist.angular.z = 0
+    twist.linear.x = 0
+    vel_pub.publish(twist)
+    sys.exit()
+
 
 if __name__ == '__main__':
     # settings = termios.tcgetattr(sys.stdin)
 
     rospy.init_node('pure_pursuit_controller')
-    vel_pub = rospy.Publisher('/vel_ctrl', Twist, queue_size=1)
+    vel_pub = rospy.Publisher('/vel_ctrl', Twist, queue_size=5)
     path_follower = PathFollower()
     path_sub = rospy.Subscriber(
         "/move_base/GlobalPlanner/plan", Path, path_follower.update_globle_path)  # 订阅全局规划器发布的路径
@@ -119,25 +128,60 @@ if __name__ == '__main__':
         key_timeout = None
 
     twist = Twist()
-    ang_vel_pid = pid.PID_t(0.2,0,0)
-    ang_pid = pid.PID_t(2.0,0,0.1)
+    ang_vel_pid = pid.PID_t(0.2, 0, 0,output_max = 999)  # 角速度控制pid
+    yaw_pid = pid.PID_t(4.0, 0, 0.2,output_max=20)  # 偏航角控制pid
+    vel_x_pid = pid.PID_t(0.8, 0.16, 0.1,int_max=10,output_max=6,sub_ctrl=True)  # 线速度x控制pid
+    start_time = rospy.get_time()
+    try:
+        vel_x = 1.0
+        signal.signal(signal.SIGINT, quit)
+        signal.signal(signal.SIGTERM, quit)
+        change_flag = 1
+        while True:
+            # if (rospy.get_time()-start_time) > 5:
+            #     start_time = rospy.get_time()
+            #     change_flag = -change_flag
+            #     if change_flag == 1:
+            #         vel_x = 1.5
+            #     elif change_flag == -1:
+            #         vel_x = 1.0
 
-    while True:
-        # key = get_key(key_timeout)
-        # if key != 's':
-        #     exit(0)
-        #     pass
-        # else:
-        # ang_vel_ctrl = ang_vel_pid.get_output(path_follower.angular_vel_z,2.0)
-        ang_ctrl_value = ang_pid.get_output(path_follower.yaw,3.14)
-        print (ang_ctrl_value)
-        twist.angular.z = ang_ctrl_value
+            # key = get_key(key_timeout,settings)
+            # if key == 'w':
+            #     vel_x += 0.5
+            # elif key == 'x':
+            #     vel_x -= 0.5
+            # elif key == 's':
+            #     vel_x = 0
+            # elif key == 'p':
+            #     print('结束')
+            #     exit(0)
+
+            # 偏航角控制
+            ang_ctrl_value =  yaw_pid.get_output(path_follower.yaw,-3.14)
+            twist.angular.z = ang_ctrl_value
+            twist.linear.x = 0
+            vel_pub.publish(twist)
+            print ("ctrl_value:%5.2f now:%5.2f"%(ang_ctrl_value,path_follower.yaw))
+
+            # 线速度控制
+            # vel_x_ctrl_value = vel_x_pid.get_output(
+            #     path_follower.linear_vel_x, vel_x)
+            # twist.angular.z = 0
+            # twist.linear.x = vel_x_ctrl_value
+            # vel_pub.publish(twist)
+            # print("ctrl_value:%5.2f now:%5.2f" %
+            #       (vel_x_ctrl_value, path_follower.linear_vel_x))
+
+            rospy.sleep(0.1) # 调整控制频率
+
+            poses = path_follower.global_path.poses
+            if len(poses) != 0:
+                # print(poses[0].pose.position.x)
+                pass
+
+    except KeyboardInterrupt:
+        twist.angular.z = 0
         twist.linear.x = 0
         vel_pub.publish(twist)
-
-        poses = path_follower.global_path.poses
-        if len(poses) != 0:
-            # print(poses[0].pose.position.x)
-            pass
-
-    # rospy.spin()
+        exit(0)
